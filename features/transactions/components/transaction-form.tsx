ef80302b0d3828646e072adebad9048d270e2bfd
David Tribugenia <david.estrelloso.tribugenia@gmail.com>
2024-07-10 21:50:56 +0800

    date bug pre fix

--- features/transactions/components/transaction-form.tsx
+++ features/transactions/components/transaction-form.tsx
@@ -1,11 +1,13 @@
 import { z } from "zod";
-import { Trash } from "lucide-react";
+import { CalendarIcon, Trash } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { insertTransactionSchema } from "@/db/schema";
+
+import { format } from "date-fns";
 
 import {
   Form,
@@ -55,6 +57,7 @@ import { DatePicker } from "@/components/DatePicker";
 import { Textarea } from "@/components/ui/textarea";
 import { AmountInput } from "@/components/AmountInput";
 import { convertAmountToMilliunits } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
 
 export const TransactionForm = ({
   id,
@@ -96,11 +99,32 @@ export const TransactionForm = ({
           render={({ field }) => (
             <FormItem>
               <FormControl>
-                <DatePicker
+                {/* <DatePicker
                   value={field.value}
                   onChange={field.onChange}
                   disabled={disabled}
-                />
+                /> */}
+                <div className="border items-center justify-center flex flex-col rounded-md">
+                  <div className="flex w-full items-center justify-center border-b py-2 rounded-lg shadow-md">
+                    <CalendarIcon className="size-4 mr-2" />
+                    {field.value ? (
+                      format(field.value, "PPP")
+                    ) : (
+                      <span>Pick a date</span>
+                    )}
+                  </div>
+                  <div className="p-4">
+                    <div className="border rounded-lg">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={disabled}
+                        initialFocus
+                      />
+                    </div>
+                  </div>
+                </div>
               </FormControl>
             </FormItem>
           )}
